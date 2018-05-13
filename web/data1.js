83041fe8f25efdbfb46c8608ebb4966777a734a7
SimeZa <simonzaucer@gmail.com>
2018-05-13 20:16:39 +0200

    popravlena vejca v data2. in dodane stranke parsane v data1

--- web/data1.js
+++ web/data1.js
@@ -1,14 +1,68 @@
 data[1] =
 {
-	"Stranka 1":
+	"DS":
 	[
-		["Poslanec 1", "Poslanec 1", "Poslanec 2"],
-		[["besedaStranke1", 100], ["asd", 86], ["beseda3", 67], ["bkjcxkjc", 59], ["lkjasd", 42], ["aaavv", 37], ["asdasdasd", 31], ["fghkjhhjk", 22], ["wertqwe", 12], ["yxcvy", 3]]
+		["France Bučar", "Spomenka Hribar", "Anton Peršak", "Rudi Šeligo"],
+		[["biti", 54712], ["da", 20850], ["ta", 20599], ["in", 15959], ["za", 15355], ["se", 14509], ["v", 12486], ["na", 10264], ["kdo", 9812], ["z", 8535]]
 	],
-	
-	"Stranka 2":
+
+	"LDS.1990":
 	[
-		["Poslanec 1", "Poslanec 2"],
-		[["besedaStranke2", 100], ["asd", 86], ["beseda3", 67], ["bkjcxkjc", 59], ["lkjasd", 42], ["aaavv", 37], ["asdasdasd", 31], ["fghkjhhjk", 22], ["wertqwe", 12], ["yxcvy", 3]]
+		["Zoran Thaler", "Franco Juri", "Mile Šetinc", "Jožef Školč", "Mojmir Ocvirk", "Janez Kopač", "Metka Mencin", "Gregor Golobič", "Andrej Verlič", "Bogomir Vaupotič", "Tone Anderlič", "Zdenka Vidovič"],
+		[["biti", 36715], ["ta", 18692], ["da", 13670], ["in", 12559], ["v", 10501], ["se", 9261], ["ki", 6801], ["za", 6581], ["na", 6491], ["z", 5306]]
+	],
+
+	"SSS":
+	[
+		["Peter Bekeš", "Borut Šuklje", "Dušan Semolič", "Jože Smole", "Viktor Žakelj", "Milan Balažic", "Darja Lavtižar"],
+		[["biti", 31830], ["ta", 16012], ["da", 12643], ["in", 12027], ["v", 10510], ["se", 7769], ["na", 6104], ["za", 6029], ["ki", 5725], ["ne", 4583]]
+	],
+
+	"SDZ-NDS":
+	[
+		["Rajko Pirnat", "Anton Tomažič"],
+		[["biti", 9391], ["ta", 3658], ["da", 3573], ["in", 2569], ["v", 2345], ["se", 1737], ["za", 1295], ["na", 1225], ["ne", 1120], ["tudi", 1117]]
+	],
+
+	"SDP":
+	[
+		["Miran Potrč", "Ciril Ribičič", "Lev Kreft", "Janez Kocijančič", "Franc Pivec", "Drago Plešivčnik", "Božo Kuharič", "Borut Pahor", "Sonja Lokar", "Franc Pipan", "Štefan Korošec"],
+		[["biti", 36344], ["da", 16021], ["ta", 15500], ["in", 14186], ["v", 11085], ["se", 8031], ["za", 6762], ["na", 6615], ["ki", 6299], ["ne", 5691]]
+	],
+
+	"SKD":
+	[
+		["Andrej Capuder", "Ciril Kolešnik", "Peter Reberc", "Alojz Peterle", "Janez Lampret", "Izidor Rejc", "Janez Gajšek", "Ignac Polajnar", "Nada Bolcar", "Štefan Kociper", "Jože Magdič"],
+		[["biti", 28956], ["ta", 12834], ["da", 9740], ["in", 9690], ["v", 7960], ["se", 6496], ["za", 4825], ["na", 4538], ["ki", 4120], ["tudi", 4101]]
+	],
+
+	"SLS":
+	[
+		["Marjan Podobnik", "France Zagožen", "Stanko Buser", "Ludvik Toplak", "Janko Halb", "Franc Potočnik", "Jože Hobič", "Ivan Pučnik", "Alojz Grabner", "Mihaela Logar", "Julij Nemanič"],
+		[["biti", 64823], ["da", 22611], ["in", 22224], ["ta", 20264], ["se", 17764], ["za", 17486], ["v", 15767], ["na", 12827], ["kdo", 12049], ["z", 10166]]
+	],
+
+	"SDSS":
+	[
+		["France Tomšič", "Emil Pintar", "Vitodrag Pukl", "Janez Janša", "Andrej Magajna", "Slavko Kmetič", "Katja Boh", "Matjaž Šinkovec", "Branko Grims", "Andrej Žorž"],
+		[["biti", 38829], ["ta", 18077], ["da", 14598], ["in", 11383], ["v", 11071], ["se", 8220], ["za", 6582], ["na", 6570], ["ki", 5846], ["ne", 5603]]
+	],
+
+	"ZS":
+	[
+		["Aleksandra Pretnar", "Leo Šešerko", "Hubert Požarnik", "Ivan Tomše", "Vane Gošnik", "Božidar Voljč", "Boris Gerl", "Žarko Pregelj"],
+		[["biti", 20324], ["ta", 8553], ["da", 7816], ["in", 6775], ["v", 6390], ["se", 4453], ["za", 3662], ["ki", 3325], ["na", 3278], ["ne", 2708]]
+	],
+
+	"Neodvisen":
+	[
+		["Roberto Battelli", "Mária Pozsonec"],
+		[["biti", 2172], ["ta", 1109], ["in", 865], ["da", 838], ["v", 716], ["se", 546], ["za", 533], ["na", 487], ["ki", 378], ["z", 311]]
+	],
+
+	"LS":
+	[
+		["Zvone Žagar", "Ivan Krefl", "Franc Golija"],
+		[["biti", 5355], ["ta", 2649], ["da", 2216], ["in", 1736], ["v", 1477], ["se", 1234], ["za", 1068], ["na", 934], ["ki", 884], ["ne", 810]]
 	]
 };
