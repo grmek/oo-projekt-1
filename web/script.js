73aa1ac8dc458ac49f032f43496c71d7bf5ba170
Rok Grmek <grmekrok@gmail.com>
2018-06-02 15:10:03 +0200

    Updated visualization 'Potek seje'.

--- web/script.js
+++ web/script.js
@@ -63,42 +63,45 @@ function addVisualizationPotekSeje(visData) {
 	var c2 = getComputedStyle(document.body).getPropertyValue('--c2');
 	
 	var minDot = 5;
-	var maxDot = 30;
-	var minSpace = 6;
+	var maxDot = 40;
+	var minSpace = 7;
 	var lineLen = 20;
+	
+	var xLegend = 670;
+	var yLegend = 50;
 	
 	var maxWords = 0;
 	var minWords = Number.MAX_VALUE;
-	for (var i = 0; i < visData.length; i++) {
-		for (var j = 0; j < visData[i][1].length; j++) {
-			if (visData[i][1][j][1] > maxWords) {
-				maxWords = visData[i][1][j][1];
+	for (var i = 0; i < visData[1].length; i++) {
+		for (var j = 0; j < visData[1][i][1].length; j++) {
+			if (visData[1][i][1][j][1] > maxWords) {
+				maxWords = visData[1][i][1][j][1];
 			}
-			if (visData[i][1][j][1] < minWords) {
-				minWords = visData[i][1][j][1];
+			if (visData[1][i][1][j][1] < minWords) {
+				minWords = visData[1][i][1][j][1];
 			}
 		}
 	}
 	
 	sizesWithoutSpacing = [];
 	maxTotalSizePerHour = 0;
-	for (var i = 0; i < visData.length; i++) {
+	for (var i = 0; i < visData[1].length; i++) {
 		var size = 0;
-		for (var j = 0; j < visData[i][1].length; j++) {
-			var dotSize = minDot + ((maxDot - minDot) * (visData[i][1][j][1] - minWords) / (maxWords - minWords));
+		for (var j = 0; j < visData[1][i][1].length; j++) {
+			var dotSize = minDot + ((maxDot - minDot) * (visData[1][i][1][j][1] - minWords) / (maxWords - minWords));
 			size += 2 * dotSize;
 		}
 		sizesWithoutSpacing.push(size);
 		
-		var tStart = new Date(visData[i][0][0]);
-		var tEnd = new Date(visData[i][0][1]);
+		var tStart = new Date(visData[1][i][0][0]);
+		var tEnd = new Date(visData[1][i][0][1]);
 		var hours = (tEnd - tStart) / 3600000;
 		
 		if (hours == 0) {
 			hours = 0.01;
 		}
 		
-		var totalSizePerHour = (size + (minSpace * (visData[i][1].length + 1))) / hours;
+		var totalSizePerHour = (size + (minSpace * (visData[1][i][1].length + 1))) / hours;
 		if (totalSizePerHour > maxTotalSizePerHour) {
 			maxTotalSizePerHour = totalSizePerHour;
 		}
@@ -108,54 +111,70 @@ function addVisualizationPotekSeje(visData) {
 	newVisualization += "<div class=\"visualization\">";
 	newVisualization += "<h2>Potek seje</h2>";
 	
-	var textSide = 1;
-	for (var i = 0; i < visData.length; i++) {
-		var tStart = new Date(visData[i][0][0]);
-		var tEnd = new Date(visData[i][0][1]);
+	for (var i = 0; i < visData[1].length; i++) {
+		var tStart = new Date(visData[1][i][0][0]);
+		var tEnd = new Date(visData[1][i][0][1]);
 		var hours = (tEnd - tStart) / 3600000;
 		var totalSize = hours * maxTotalSizePerHour;
-		var space = (totalSize - sizesWithoutSpacing[i]) / (visData[i][1].length + 1);
+		var space = (totalSize - sizesWithoutSpacing[i]) / (visData[1][i][1].length + 1);
 		
 		// tStart
 		newVisualization += tStart.getDay() + "." + tStart.getMonth() + "." + tStart.getFullYear() + " " + tStart.getHours() + ":" + ("0" + tStart.getMinutes()).slice(-2) + "<br>";
 		
 		// svg begin
-		newVisualization += "<svg width=\"500\" height=\"" + totalSize + "\" style=\"fill: " + c1 + ";\">"
+		newVisualization += "<svg width=\"800\" height=\"" + totalSize + "\" style=\"fill: " + c1 + ";\">"
+		
+		// legend (for first element only)
+		if (i == 0) {
+			newVisualization += "<line x1=\"" + xLegend + "\" y1=\"" + yLegend + "\" x2=\"" + xLegend + "\" y2=\"" + (yLegend + 22) + "\" style=\"stroke: " + c1 + "; stroke-width: 2;\"/>"
+			newVisualization += "<text x=\"" + (xLegend + 25) + "\" y=\"" + (yLegend + 18) + "\" text-anchor=\"start\">Čas seje.</text>";
+			
+			newVisualization += "<circle cx=\"" + xLegend + "\" cy=\"" + (yLegend + 52) + "\" r=\"5\" style=\"fill: " + c2 + ";\"/>";
+			newVisualization += "<text x=\"" + (xLegend + 25) + "\" y=\"" + (yLegend + 58) + "\" text-anchor=\"start\">Št. besed.</text>";
+		}
 		
 		// main line
-		newVisualization += "<line x1=\"250\" y1=\"0\" x2=\"250\" y2=\"" + totalSize + "\" style=\"stroke: " + c1 + "; stroke-width: 2;\"/>"
+		newVisualization += "<line x1=\"400\" y1=\"0\" x2=\"400\" y2=\"" + totalSize + "\" style=\"stroke: " + c1 + "; stroke-width: 2;\"/>"
 		
 		var position = space;
-		for (var j = 0; j < visData[i][1].length; j++) {
-			var dotSize = minDot + ((maxDot - minDot) * (visData[i][1][j][1] - minWords) / (maxWords - minWords));
+		for (var j = 0; j < visData[1][i][1].length; j++) {
+			var dotSize = minDot + ((maxDot - minDot) * (visData[1][i][1][j][1] - minWords) / (maxWords - minWords));
 			position += dotSize;
 			
 			// dot
-			newVisualization += "<circle cx=\"250\" cy=\"" + position + "\" r=\"" + dotSize + "\" style=\"fill: " + c2 + ";\"/>";
+			newVisualization += "<circle cx=\"400\" cy=\"" + position + "\" r=\"" + dotSize + "\" style=\"fill: " + c2 + ";\"/>";
+			
+			// text side
+			var president = false;
+			for (var k = 0; k < visData[0].length; k++) {
+				if (visData[1][i][1][j][0] == visData[0][k]) {
+					president = true;
+					break;
+				}
+			}
 			
 			// text
-			if (textSide == 1) {
-				newVisualization += "<line x1=\"250\" y1=\"" + position + "\" x2=\"" + (250 + (dotSize + lineLen)) + "\" y2=\"" + position + "\" style=\"stroke: " + c2 + "; stroke-width: 2;\"/>"
-				if (visData[i][1][j][0] in data[2]) {
-					newVisualization += "<text x=\"" + (250 + (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"start\" onclick=\"jumpTo(2, '" + visData[i][1][j][0] + "')\" style=\"cursor: pointer;\">" + visData[i][1][j][0] + "</text>";
+			if (president) {
+				newVisualization += "<line x1=\"400\" y1=\"" + position + "\" x2=\"" + (400 - (dotSize + lineLen)) + "\" y2=\"" + position + "\" style=\"stroke: " + c2 + "; stroke-width: 2;\"/>"
+				if (visData[1][i][1][j][0] in data[2]) {
+					newVisualization += "<text x=\"" + (400 - (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"end\" onclick=\"jumpTo(2, '" + visData[1][i][1][j][0] + "')\" style=\"cursor: pointer;\">" + visData[1][i][1][j][0] + "</text>";
 				}
 				else {
-					newVisualization += "<text x=\"" + (250 + (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"start\" style=\"cursor: default;\">" + visData[i][1][j][0] + "</text>";
+					newVisualization += "<text x=\"" + (400 - (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"end\" style=\"cursor: default;\">" + visData[1][i][1][j][0] + "</text>";
 				}
 			}
 			else {
-				newVisualization += "<line x1=\"250\" y1=\"" + position + "\" x2=\"" + (250 - (dotSize + lineLen)) + "\" y2=\"" + position + "\" style=\"stroke: " + c2 + "; stroke-width: 2;\"/>"
-				if (visData[i][1][j][0] in data[2]) {
-					newVisualization += "<text x=\"" + (250 - (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"end\" onclick=\"jumpTo(2, '" + visData[i][1][j][0] + "')\" style=\"cursor: pointer;\">" + visData[i][1][j][0] + "</text>";
+				newVisualization += "<line x1=\"400\" y1=\"" + position + "\" x2=\"" + (400 + (dotSize + lineLen)) + "\" y2=\"" + position + "\" style=\"stroke: " + c2 + "; stroke-width: 2;\"/>"
+				if (visData[1][i][1][j][0] in data[2]) {
+					newVisualization += "<text x=\"" + (400 + (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"start\" onclick=\"jumpTo(2, '" + visData[1][i][1][j][0] + "')\" style=\"cursor: pointer;\">" + visData[1][i][1][j][0] + "</text>";
 				}
 				else {
-					newVisualization += "<text x=\"" + (250 - (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"end\" style=\"cursor: default;\">" + visData[i][1][j][0] + "</text>";
+					newVisualization += "<text x=\"" + (400 + (dotSize + 1.5 * lineLen)) + "\" y=\"" + (position + 5) + "\" text-anchor=\"start\" style=\"cursor: default;\">" + visData[1][i][1][j][0] + "</text>";
 				}
 			}
 			
 			position += dotSize;
 			position += space;
-			textSide *= -1;
 		}
 		
 		// svg end
@@ -163,7 +182,7 @@ function addVisualizationPotekSeje(visData) {
 		
 		// tEnd
 		newVisualization += tEnd.getDay() + "." + tEnd.getMonth() + "." + tEnd.getFullYear() + " " + tEnd.getHours() + ":" + ("0" + tEnd.getMinutes()).slice(-2) + "<br>";
-		if (i < (visData.length - 1)) {
+		if (i < (visData[1].length - 1)) {
 			newVisualization += "<br><br><br>";
 		}
 	}
